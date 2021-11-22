b7ecf028193c0d6634c6b4a28b00dfd11d14c8bb
seongwoo <he1236@ajou.ac.kr>
2021-11-22 19:40:33 +0900

    making loginButton

--- app.js
+++ app.js
@@ -7,9 +7,21 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
+
+//로그인 버튼 클릭
 function onLoginBtnClick(){
-    console.log("Hi",loginInput.value);
-    console.log("click!!");
+    const userName = loginInput.value;
+
+    if(userName===""){
+        console.log("Plz write your name");
+    } else if(userName.length > 15){
+        console.log("Your name is too long!")
+    }
+    else{
+        console.log("Hi",userName);
+        console.log("clicked!")
+    }
 }
+    
 
 loginButton.addEventListener("click",onLoginBtnClick);
